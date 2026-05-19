eaf4ff3e7e7320483c6e779f003fb68c803aa775
emmieshukie-coder <emmieshukie@gmail.com>
2026-05-19 07:28:13 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,7 @@
-const CACHE_NAME = 'jobai-v1';
-const urlsToCache = ['/', '/public/manifest.json'];
-
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
