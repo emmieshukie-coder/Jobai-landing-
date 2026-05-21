48e337eb9f664bd0fa9d9fdfe136fe3207bde6fa
emmieshukie-coder <emmieshukie@gmail.com>
2026-05-21 08:29:23 +0300

    Add dynamic import for node-fetch

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
 const express = require('express');
 const multer = require('multer');
 const { v2 as cloudinary } = require('cloudinary');
