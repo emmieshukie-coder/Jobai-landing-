7150b1b07cbffc2ca9283b2df6763d76c61115a7
emmieshukie-coder <emmieshukie@gmail.com>
2026-05-24 23:08:54 +0300

    Create robots.ts

---
+++ robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://jobai-landing.onrender.com/sitemap.xml',
+  }
+}
