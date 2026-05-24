fa01c40e5a0e490bd55073a6274486563c8f0ed0
emmieshukie-coder <emmieshukie@gmail.com>
2026-05-24 21:15:58 +0300

    Create sitemap.js

---
+++ sitemap.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/sitemap.xml', (req, res) => {
+  res.header('Content-Type', 'application/xml');
+  res.send(`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://jobai-landing.onrender.com/</loc>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>`);
+});
+
+module.exports = router;
